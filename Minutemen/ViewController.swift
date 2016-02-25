e3361a73bd5db809b86796370203650ba4bf25a2
Luigi Freitas Cruz <luigifcruz@icloud.com>
2016-02-24 21:25:30 -0300

    Initial Commit

---
+++ Minutemen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Minutemen
+//
+//  Created by Luigi Freitas Cruz on 2/24/16.
+//  Copyright © 2016 Luigi Freitas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
